42b474112359f6ece480369dbda46730b13b1abf
denisprokhorchik@hotmail.com <denisprokhorchik@hotmail.com>
2020-06-13 23:18:05 +0300

    adede new fix (DummyEmailSender)

--- source/O2.Auth.Web/Startup.cs
+++ source/O2.Auth.Web/Startup.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using O2.Auth.Web.Data;
 
 namespace O2.Auth.Web
@@ -25,6 +23,7 @@ namespace O2.Auth.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddDbContext<AuthDbContext>(options =>
             {
                 options.UseSqlServer(_configuration.GetConnectionString("AuthDbContext"));
@@ -33,6 +32,7 @@ namespace O2.Auth.Web
             services.AddIdentity<O2User,IdentityRole>()
                 .AddEntityFrameworkStores<AuthDbContext>()
                 .AddDefaultTokenProviders();
+            services.AddSingleton<IEmailSender, DummyEmailSender>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,10 +43,24 @@ namespace O2.Auth.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+        }
+
+        internal class DummyEmailSender : IEmailSender
+        {
+            private readonly ILogger<DummyEmailSender> _logger;
+
+            public DummyEmailSender(ILogger<DummyEmailSender> logger)
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                _logger = logger;
+            }
+            public Task SendEmailAsync(string email, string subject, string htmlMessage)
+            {
+                _logger.LogWarning("Dummy IEmailSender implementation is being used!!!");
+                return Task.CompletedTask;
+            }
         }
     }
 }
