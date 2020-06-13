8e6355f19b501388c76f43d492605f9e3a810188
denisprokhorchik@hotmail.com <denisprokhorchik@hotmail.com>
2020-06-13 22:50:58 +0300

    added ef core

---
+++ source/O2.Auth.Web/Migrations/20200613195009_RemovedSampleColumnToO2User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace O2.Auth.Web.Migrations
+{
+    public partial class RemovedSampleColumnToO2User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sample",
+                schema: "dbo",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sample",
+                schema: "dbo",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+    }
+}
--- source/O2.Auth.Web/Startup.cs
+++ source/O2.Auth.Web/Startup.cs
@@ -5,16 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using O2.Auth.Web.Data;
 
 namespace O2.Auth.Web
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AuthDbContext>(options =>
+            {
+                options.UseSqlServer(_configuration.GetConnectionString("AuthDbContext"));
+            } );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
